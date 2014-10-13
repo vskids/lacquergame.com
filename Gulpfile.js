adec33c51676745be25633b2b24ba6229dc080fc
Slipp Douglas Thompson <git@slippyd.com>
2014-10-13 05:26:07 -0400

    Converted to using Less via gulp-less
    • Installed gulp-less via NPM dev dep.
    • Added Gulpfile `build-less` task, as per https://www.npmjs.org/package/gulp-less
    • Created new `less/styles.less`, which imports `bootstrap.less`, `non-responsive.css`, and new `mixins.less`, `colors.less`, `main.less`.
    	• New `mixins.less`, `colors.less`, `main.less` are all currently empty and intended for custom mixins, custom color vars, and main styles (respectively).
    	• Now `index.html` just links to the composite `styles.css`.
    	• Ran `gulp build-less`, updating `css/styles.css`.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp-help')(
 	require('gulp')
 );
+var less = require('gulp-less');
 
-gulp.task('default', function() {
-	// place code for your default task here
+
+// Compiles LESS › CSS 
+gulp.task('build-less', function () {
+	gulp.src('./less/styles.less').pipe(
+		less()
+	).pipe(
+		gulp.dest('./css/')
+	);
 });
