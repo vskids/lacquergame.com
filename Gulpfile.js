8ef43c9eb3082d324116a2545cb1c20cf8fc54f4
Slipp Douglas Thompson <git@slippyd.com>
2014-10-13 05:40:34 -0400

    Added gulp-sourcemaps for Less/CSS
    • Installed `gulp-sourcemaps` NPM package.
    • Added codestuffs to Gulpfile.js to do sourcemaps for Less/CSS, also per https://www.npmjs.org/package/gulp-less
    • Rebuilt `styles.css`, which added `styles.css.map`.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,12 +2,17 @@ var gulp = require('gulp-help')(
 	require('gulp')
 );
 var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
 
 
 // Compiles LESS › CSS 
 gulp.task('build-less', function () {
 	gulp.src('./less/styles.less').pipe(
+		sourcemaps.init()
+	).pipe(
 		less()
+	).pipe(
+		sourcemaps.write('./')
 	).pipe(
 		gulp.dest('./css/')
 	);
