f3c463a35f044e671cc680c62145dd6b8d132e76
Slipp Douglas Thompson <git@slippyd.com>
2014-10-13 20:03:03 -0400

    Added PATHS constant
    • Added a `PATHS` constant with paths to dirs per usage (dev vs. browser) and type (less, css, etc.).
    	» Inspired by http://blog.mdstn.com/using-laravel-gulp-bootstrap-and-less/
    	» Not sure if the `const` keyword enforces anything… but it does compile and run.
    	• Updated usages of paths to use the `PATHS` entries.

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,16 +5,26 @@ var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 
 
+const PATHS = {
+	'dev': {
+		'less': './less/'
+	},
+	'browser': {
+		'css': './css/'
+	}
+};
+
+
 gulp.task('build-less', "Compiles LESS › CSS",
 	function () {
-		gulp.src('./less/styles.less').pipe(
+		gulp.src(PATHS.dev.less+'styles.less').pipe(
 			sourcemaps.init()
 		).pipe(
 			less()
 		).pipe(
 			sourcemaps.write('./')
 		).pipe(
-			gulp.dest('./css/')
+			gulp.dest(PATHS.browser.css)
 		);
 	},
 	{ "aliases": ['less'] }
