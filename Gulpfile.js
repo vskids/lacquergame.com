d2229b04ad11c655e77e3d71abdaa07cd4289e2b
Slipp Douglas Thompson <git@slippyd.com>
2014-10-13 20:43:12 -0400

    Added task aliases
    • Added a `addGulpTaskAlias()` helper function, which calls `gulp.task` with basic help text in the format of “(alias for «originalTask»)”.
    • Added a addGulpTaskAlias line afterwards aliasing `build-less` to `less`.
    	» Decide to put these at the end of the file with the intent that other tasks should call the real names of tasks, not alias-names.  Not sure if this'll be enforced by Gulp (since task refs are just strings), but eh.
    	• Removed extra indentation and `aliases` options from the `build-less` task.

--- Gulpfile.js
+++ Gulpfile.js
@@ -15,22 +15,27 @@ const PATHS = {
 };
 
 
-gulp.task('build-less', "Compiles LESS › CSS",
-	function () {
-		gulp.src(PATHS.dev.less+'styles.less').pipe(
-			sourcemaps.init()
-		).pipe(
-			less()
-		).pipe(
-			sourcemaps.write('./')
-		).pipe(
-			gulp.dest(PATHS.browser.css)
-		);
-	},
-	{ "aliases": ['less'] }
-);
+gulp.task('build-less', "Compiles LESS › CSS", function () {
+	gulp.src(PATHS.dev.less+'styles.less').pipe(
+		sourcemaps.init()
+	).pipe(
+		less()
+	).pipe(
+		sourcemaps.write('./')
+	).pipe(
+		gulp.dest(PATHS.browser.css)
+	);
+});
 
 
 gulp.task('watch', "Automatically re-builds changes LESS files", function () {
 	gulp.watch(PATHS.dev.less+'**/*.less', ['build-less']);
 });
+
+
+function addGulpTaskAlias(alias, originalTask)
+{
+	gulp.task(alias, "(alias for `"+originalTask+"`)", [originalTask]);
+}
+
+addGulpTaskAlias('less', 'build-less');
